ad88d4fab911581e156fbc8eedcc900aa1419722
Christoffer Frisk <christofffer@gmail.com>
2026-05-26 09:53:39 +0200

    ci: run docker containers as host UID to fix bind-mount permissions

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -12,19 +12,20 @@ if ! docker image inspect "$IMAGE" >/dev/null 2>&1; then
 fi
 
 DATA="$ROOT/test/data"
+DOCKER_USER="$(id -u):$(id -g)"
 
-docker run --rm -v "$DATA":/data --entrypoint samtools "$IMAGE" sort -o /data/sampleA.bam /data/sampleA.sam
-docker run --rm -v "$DATA":/data --entrypoint samtools "$IMAGE" sort -o /data/sampleB.bam /data/sampleB.sam
+docker run --rm --user "$DOCKER_USER" -v "$DATA":/data --entrypoint samtools "$IMAGE" sort -o /data/sampleA.bam /data/sampleA.sam
+docker run --rm --user "$DOCKER_USER" -v "$DATA":/data --entrypoint samtools "$IMAGE" sort -o /data/sampleB.bam /data/sampleB.sam
 
 printf "/data/sampleA.bam\n/data/sampleB.bam\n" > "$DATA/bams.txt"
 
-docker run --rm -v "$DATA":/data "$IMAGE" \
+docker run --rm --user "$DOCKER_USER" -v "$DATA":/data "$IMAGE" \
     --region testchr:400-600 \
     --ref /data/ref.fa \
     --bam-list /data/bams.txt \
     --out /data/out.vcf.gz
 
-n=$(docker run --rm -v "$DATA":/data --entrypoint bcftools "$IMAGE" view -H /data/out.vcf.gz | wc -l)
+n=$(docker run --rm --user "$DOCKER_USER" -v "$DATA":/data --entrypoint bcftools "$IMAGE" view -H /data/out.vcf.gz | wc -l)
 echo "integration test called $n variant site(s)"
 [[ "$n" -ge 1 ]] || { echo "FAIL: expected >=1 variant, got $n" >&2; exit 1; }
 echo "integration test PASSED"
